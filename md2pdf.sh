692d3143b601d777e0989b06f6643a2adfcc5604
yahya jabary <61852663+sueszli@users.noreply.github.com>
2024-04-11 01:30:56 +0200

    Update md2pdf.sh

--- md2pdf.sh
+++ md2pdf.sh
@@ -1,3 +1,41 @@
-pandoc --read=markdown --write=latex --output=testdoc.tex --standalone test.md
+brew install pandoc
+brew tap homebrew/cask
+brew install --cask basictex
+eval "$(/usr/libexec/path_helper)"
 
-# alternatively: hack together headless obsidian export
+# update $PATH to include `/usr/local/texlive/2022basic/bin/universal-darwin`
+export PATH=$PATH:/usr/local/texlive/2022basic/bin/universal-darwin
+
+# check if successfully added to $PATH
+echo $PATH | grep -q "/usr/local/texlive/2022basic/bin/universal-darwin" && echo "found" || echo "not found"
+
+sudo tlmgr update --self
+sudo tlmgr install texliveonfly
+sudo tlmgr install xelatex
+sudo tlmgr install adjustbox
+sudo tlmgr install tcolorbox
+sudo tlmgr install collectbox
+sudo tlmgr install ucs
+sudo tlmgr install environ
+sudo tlmgr install trimspaces
+sudo tlmgr install titling
+sudo tlmgr install enumitem
+sudo tlmgr install rsfs
+sudo tlmgr install geometry
+
+pandoc \
+    --read=markdown --write=latex --output=test.pdf \
+    --pdf-engine=xelatex \
+    --variable geometry:margin=20mm \
+    --variable fontsize=6pt \
+    test.md
+
+# alternatively use a template: https://github.com/Wandmalfarbe/pandoc-latex-template/
+sudo tlmgr install collection-fontsrecommended
+sudo tlmgr install adjustbox babel-german background bidi collectbox csquotes everypage filehook footmisc footnotebackref framed fvextra letltxmacro ly1 mdframed mweights needspace pagecolor sourcecodepro sourcesanspro titling ucharcat ulem unicode-math upquote xecjk xurl zref
+
+pandoc \
+    --read=markdown --write=latex --output=test.pdf \
+    --pdf-engine=xelatex \
+    --template="./eisvogel.tex" \
+    test.md
