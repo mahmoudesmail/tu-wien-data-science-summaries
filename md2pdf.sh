5ac87867ce75c7f422ca28ece1be959a604da98a
yahya jabary <61852663+sueszli@users.noreply.github.com>
2024-04-24 16:32:33 +0200

    Update md2pdf.sh

--- md2pdf.sh
+++ md2pdf.sh
@@ -23,16 +23,16 @@ sudo tlmgr install enumitem
 sudo tlmgr install rsfs
 sudo tlmgr install geometry
 
-pandoc \
-    --read=markdown --write=latex --output=test.pdf \
-    --pdf-engine=xelatex \
-    --variable geometry:margin=20mm \
-    --variable fontsize=6pt \
-    test.md
+# pandoc \
+#     --read=markdown --write=latex --output=test.pdf \
+#     --pdf-engine=xelatex \
+#     --variable geometry:margin=20mm \
+#     --variable fontsize=6pt \
+#     test.md
 
 # alternatively use a template: https://github.com/Wandmalfarbe/pandoc-latex-template/
-sudo tlmgr install collection-fontsrecommended
-sudo tlmgr install adjustbox babel-german background bidi collectbox csquotes everypage filehook footmisc footnotebackref framed fvextra letltxmacro ly1 mdframed mweights needspace pagecolor sourcecodepro sourcesanspro titling ucharcat ulem unicode-math upquote xecjk xurl zref
+# sudo tlmgr install collection-fontsrecommended
+# sudo tlmgr install adjustbox babel-german background bidi collectbox csquotes everypage filehook footmisc footnotebackref framed fvextra letltxmacro ly1 mdframed mweights needspace pagecolor sourcecodepro sourcesanspro titling ucharcat ulem unicode-math upquote xecjk xurl zref
 
 # run
 pandoc --read=markdown --write=latex --pdf-engine=xelatex --variable geometry:margin=20mm --variable documentclass:extarticle --variable fontsize:8pt --output=test.pdf test.md
